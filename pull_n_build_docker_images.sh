c343cdea86be3d0b19460616daf29cb43bdeb6e2
Rurtle Coder <rurtle.coder@gmail.com>
2024-06-16 17:17:00 +0530

    Spelling mistake

--- pull_n_build_docker_images.sh
+++ pull_n_build_docker_images.sh
@@ -4,12 +4,12 @@ echo "!!!!~~~~~~~~~~~~Trying to run this script from Jenkins pipeline~~~~~~~~~~!
 
 echo "Pulling the latest /user/bin/docker image for Postgres"
 # Pull the Postgres /user/bin/docker image from /user/bin/dockerHub
-/user/bin/docker pull postgres:latest
+/usr/bin/docker pull postgres:latest
 
 echo "Building the latest /user/bin/docker image for Postgres"
 # Build it
-/user/bin/docker build -t my_postgres_image .
+/usr/bin/docker build -t my_postgres_image .
 
 echo "Running the latest /user/bin/docker image for Postgres"
 # Run it
-/user/bin/docker run -d -p 5432:5432 --name my_postgres_container my_postgres_image
+/usr/bin/docker run -d -p 5432:5432 --name my_postgres_container my_postgres_image

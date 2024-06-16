fdbf4ba5950dd651cfa69d060221189b7b957395
Rurtle Coder <rurtle.coder@gmail.com>
2024-06-16 17:16:02 +0530

    Docker - command not found

--- pull_n_build_docker_images.sh
+++ pull_n_build_docker_images.sh
@@ -2,14 +2,14 @@
 
 echo "!!!!~~~~~~~~~~~~Trying to run this script from Jenkins pipeline~~~~~~~~~~!!!!"
 
-echo "Pulling the latest Docker image for Postgres"
-# Pull the Postgres docker image from DockerHub
-docker pull postgres:latest
+echo "Pulling the latest /user/bin/docker image for Postgres"
+# Pull the Postgres /user/bin/docker image from /user/bin/dockerHub
+/user/bin/docker pull postgres:latest
 
-echo "Building the latest Docker image for Postgres"
+echo "Building the latest /user/bin/docker image for Postgres"
 # Build it
-docker build -t my_postgres_image .
+/user/bin/docker build -t my_postgres_image .
 
-echo "Running the latest Docker image for Postgres"
+echo "Running the latest /user/bin/docker image for Postgres"
 # Run it
-docker run -d -p 5432:5432 --name my_postgres_container my_postgres_image
+/user/bin/docker run -d -p 5432:5432 --name my_postgres_container my_postgres_image

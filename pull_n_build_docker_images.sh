a989325f5a5c8db835ec03cf625aa345e1402ec8
Rurtle Coder <rurtle.coder@gmail.com>
2024-06-16 17:14:11 +0530

    Postgres image pull, build & run

--- pull_n_build_docker_images.sh
+++ pull_n_build_docker_images.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo "!!!!~~~~~~~~~~~~Trying to run this script from Jenkins pipeline~~~~~~~~~~!!!!"
+
+echo "Pulling the latest Docker image for Postgres"
+# Pull the Postgres docker image from DockerHub
+docker pull postgres:latest
+
+echo "Building the latest Docker image for Postgres"
+# Build it
+docker build -t my_postgres_image .
+
+echo "Running the latest Docker image for Postgres"
+# Run it
+docker run -d -p 5432:5432 --name my_postgres_container my_postgres_image
